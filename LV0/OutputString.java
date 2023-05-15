2960df869f687c62675a52785e4933ed27463051
Recyclingbottle <hoholeo382@gmail.com>
2023-05-15 13:58:53 +0900

    try-catch 문 추가 경고문 제거

--- LV0/OutputString.java
+++ LV0/OutputString.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class OutputString {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        System.out.println(a);
+        try (Scanner sc = new Scanner(System.in)) {
+            String a = sc.next();
+            System.out.println(a);
+        }
     }
 }
--- LV0/outputtingab.java
+++ LV0/outputtingab.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class outputtingab {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
-        System.out.printf("a = %d\nb = %d\n",a,b);
+            System.out.printf("a = %d\nb = %d\n",a,b);
+        }
     }
 }
