b67f69124375dda65e47b1a974dd489408d67562
Recyclingbottle <hoholeo382@gmail.com>
2023-05-19 23:07:13 +0900

    마지막 두 원소

---
+++ LV0/LastTwoElements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer;
+        int length = num_list.length;
+        if (num_list[length - 1] > num_list[length - 2]) {
+            answer = new int[length + 1];
+            answer[length] = num_list[length - 1] - num_list[length - 2];
+        } else {
+            answer = new int[length + 1];
+            answer[length] = num_list[length - 1] * 2;
+        }
+        for (int i = 0; i < length; i++) {
+            answer[i] = num_list[i];
+        }
+
+        return answer;
+    }
+}
